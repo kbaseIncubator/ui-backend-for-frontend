495df5da070521bf4592cf55fc0697263876117c
Akiyo <flying.unicorn.sf@gmail.com>
2019-06-24 14:41:15 -0700

    add more files

--- Test/test.py
+++ Test/test.py
@@ -9,13 +9,13 @@ class WidgetTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-        json_path = os.path.join( THIS_FOLDER, 'myprofile.json')
+        json_path = os.path.join( THIS_FOLDER, 'testprofile.json')
         with open(json_path) as json_file:
             cls.data = json.load(json_file)
 
     def test_userProfile(self):
        
-        self.assertEqual(get_userProfile('amarukawa'), json.dumps(self.data))
+        self.assertEqual(get_userProfile('soratest'), json.dumps(self.data))
 
 
 if __name__ == '__main__':
