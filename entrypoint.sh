52cadc24e66c54a48eedad5f743d4231401cf596
Shane Canon <scanon@lbl.gov>
2019-06-28 12:58:12 -0700

    Add report and dynamic service flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,10 @@
 if [ "z${1}" == "ztest" ] ; then
   echo "No test"
   exit
+elif [ "z${1}" == "zreport" ] ; then
+  echo "Report"
+  echo "{}" > ./work/compile_report.json
+  exit
 else
   sh ./scripts/start_server.sh
 fi
